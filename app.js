c9f107874a9be4e5f716da6eda1cb932875fa8f2
FuzzySid <sid.cd.varma@gmail.com>
2023-04-23 16:14:24 +0530

    Created project

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const PORT = process.env.PORT || 8080
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/cors') {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Hello, CORS!' }));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
+server.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`)
+});
