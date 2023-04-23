e882208c3fe5622219dad6b3b0e6af1e228ea36c
FuzzySid <sid.cd.varma@gmail.com>
2023-04-23 16:31:11 +0530

    app.js: commented cors rule

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const PORT = process.env.PORT || 8080
 
 const server = http.createServer((req, res) => {
   if (req.url === '/cors') {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    if (req.method === 'OPTIONS') {
-      res.writeHead(204);
-      res.end();
-      return;
-    }
+    // if (req.method === 'OPTIONS') {
+    //   res.writeHead(204);
+    //   res.end();
+    //   return;
+    // }
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Hello, CORS!' }));
   } else {
     res.writeHead(404);
